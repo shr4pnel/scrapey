ad15628c97f08f1b16e61d569f7b0eedb54dc039
benji.aird <benji.aird@mediatonic.co.uk>
2023-02-12 15:12:34 +0000

    Reorganised code so that functionality to message scraping & wrangling lives in the new 'messages' module. Added 'visualiation' module for handling visualisation tasks. Moved demo code to main.py.

---
+++ visualisation.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+
+def get_bar_graph(x_plot, y_plot, title):
+    plt.figure(figsize=(3, 3))
+    plt.scatter(x_plot, y_plot)
+    plt.suptitle(title)
+    return plt
